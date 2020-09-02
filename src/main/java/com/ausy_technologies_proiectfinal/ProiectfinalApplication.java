f58c8e477e0a4698bbf81fd92bc4004ec1d33eb1
Georgiana Petrus <georgiana.petrus91@gmail.com>
2020-09-02 17:02:43 +0300

    First commit

---
+++ src/main/java/com/ausy_technologies_proiectfinal/ProiectfinalApplication.java
@@ -0,0 +1,13 @@
+package com.ausy_technologies_proiectfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ausy_technologies_proiectfinal/ProiectfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ausy_technologies_proiectfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
